bc9b4574db8501c96f966c2a6b8aed05807eb9bf
Mariam Segaa <119626206+mariamsegaa@users.noreply.github.com>
2025-05-03 22:38:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,16 +16,25 @@ form.addEventListener('submit', (e) => {
         <p>${description}</p>
         <p><strong>Date:</strong> ${new Date(date).toDateString()}</p>
     `;
-
     eventCard.innerHTML = eventContent;
     timelineContainer.appendChild(eventCard);
-
-    form.reset();
+    
+    document.getElementById('eventTitle').value = '';
+    document.getElementById('eventDescription').value = '';
+    document.getElementById('eventDate').value = '';
 });
 
 
+const reset = document.getElementById('reset');
 reset.onclick = () => confirm("Clear all events?") && (
     events = [],
     timelineContainer.innerHTML = '',
     localStorage.removeItem('timelineEvents')
 );
+
+const darkToggle = document.getElementById('toggle');
+
+darkToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
